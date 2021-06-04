e2d3ee7a512bd1b16358ff5582999f4862ebf46f
rhie-coder <quotia72@naver.com>
2021-06-04 15:34:23 +0900

    multer is finished, and study mongodb

--- tester/main.js
+++ tester/main.js
@@ -2,34 +2,45 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
-const multer = require('multer');
+const { MongoClient } = require("mongodb");
 
 app.use('/static', express.static(path.join(__dirname, 'public')))
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
-const storage = multer.diskStorage({
-  destination: function (req, file, callback) {
-    callback(null, '/uploads')
-  },
-  filename: function (req, file, callback) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    callback(null, file.fieldname + '-' + uniqueSuffix)
-  }
-})
+/*** 데이터베이스 ***/
+const uri = "mongodb://127.0.0.1:27017";
 
-const upload = multer({ 
-  storage: storage,
-  limits: {
-    files: 10,
-    fileSize: 1024 * 1024 * 1024
-  }
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-app.post('/stats', upload.array(), function(req, res){
-  console.log(req.files);
-  res.send(req.files);
-})
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Establish and verify connection
+    await client.db("local").command({ ping: 1 });
+    console.log("MongoDB server has received heart beat~!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+function connectDB(){
+  client.connect((err,db)=>{
+    if(err) throw err;
+
+    console.log("Connected successfully to MongoDB server");
+
+    database = db;
+  })
+}
+
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
+  run().catch(console.dir);
+  // connectDB();
 })
+
